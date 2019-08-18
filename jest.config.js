6538822347318b87050cdf652ae0c8cda8754110
Cole Schneider <colekschneider@gmail.com>
2019-08-18 09:20:27 -0700

    cov

--- jest.config.js
+++ jest.config.js
@@ -1,32 +1,25 @@
 module.exports = {
-  cacheDirectory: "<rootDir>/jestCache",
-    collectCoverageFrom: [
-        "**/*.{ts,tsx}",
-        "!**/node_modules/**",
-    ],
-    coveragePathIgnorePatterns: [
-      "/types/",
-      "/src/images",
-      "/src/App.tsx",
-      "/src/Routes.tsx",
-      "/src/index.tsx",
-      "/src/serviceWorker.ts",
-      "src/Common/LazyRoute/LazyRoute.tsx"
+  cacheDirectory: '<rootDir>/jestCache',
+  collectCoverageFrom: ['**/*.{ts,tsx}', '!**/node_modules/**'],
+  coveragePathIgnorePatterns: [
+    '/types/',
+    '/src/images',
+    '/src/App.tsx',
+    '/src/Routes.tsx',
+    '/src/index.tsx',
+    '/src/serviceWorker.ts',
+    'src/Common/LazyRoute/LazyRoute.tsx',
   ],
-  coverageThreshold: {
-      global: {
-          branches: 100,
-          functions: 100,
-          lines: 100,
-          statements: 100
-      }
-  },
-  modulePathIgnorePatterns: ["<rootDir>/build/"],
-  coverageReporters: ["json-summary", "text", "lcov"],
-    roots: ['<rootDir>/src'],
-    transform: {
-      '^.+\\.tsx?$': 'ts-jest',
-    },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  }
+  coverageThreshold: {global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },},
+  modulePathIgnorePatterns: ['<rootDir>/build/'],
+  coverageReporters: ['json-summary', 'text', 'lcov'],
+  roots: ['<rootDir>/src'],
+  transform: {'^.+\\.tsx?$': 'ts-jest',},
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
